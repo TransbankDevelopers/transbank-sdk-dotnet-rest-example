2f5b2d65277c1265aadfa525c051e7990a4fb534
Felipe Fiebig <felipe.fiebig@continuum.cl>
2019-06-25 12:22:29 -0400

    Init with Plus Transaction Create

---
+++ transbank-sdk-dotnet-rest-example/Controllers/WebpayPlusController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Transbank.Webpay.WebpayPlus;
+
+namespace transbanksdkdotnetrestexample.Controllers
+{
+    public class WebpayPlusController : Controller
+    {
+        public ActionResult NormalCreate()
+        {
+            var random = new Random();
+            var buyOrder = random.Next(999999999).ToString();
+            var sessionId = random.Next(999999999).ToString();
+            var amount = random.Next(1000,999999);
+            UrlHelper urlHelper = new UrlHelper(ControllerContext.RequestContext);
+            var returnUrl = urlHelper.Action("NormalReturn", "WebpayPlus", null, Request.Url.Scheme);
+            var result = Transaction.Create(buyOrder, sessionId, amount, returnUrl);
+
+            ViewBag.BuyOrder = buyOrder;
+            ViewBag.SessionId = sessionId;
+            ViewBag.Amount = amount;
+            ViewBag.ReturnUrl = returnUrl;
+            ViewBag.Result = result;
+            ViewBag.Action = result.Url;
+            ViewBag.Token = result.Token;
+            return View ();
+        }
+
+        public ActionResult NormalReturn()
+        {
+            return View();
+        }
+    }
+}
