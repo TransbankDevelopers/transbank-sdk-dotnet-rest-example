d6324cfce335ef0c11186584ea54b0077e3c52e5
afiebig <alfredo.fiebig@continuum.cl>
2019-07-22 17:14:28 -0400

    Add Init and Finish Inscription

---
+++ transbank-sdk-dotnet-rest-example/Controllers/OneclickController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Transbank.Webpay.Oneclick;
+
+namespace transbanksdkdotnetrestexample.Controllers
+{
+    public class OneclickController : Controller
+    {
+        public ActionResult InscriptionStart()
+        {
+            var UserName = "Pepito Continuum";
+            var Email = "pepito@continuum.co";
+
+            UrlHelper urlHelper = new UrlHelper(ControllerContext.RequestContext);
+            var ResponseURL = urlHelper.Action("InscriptionFinish", "Oneclick", null, Request.Url.Scheme);
+            var result = Inscription.Start(UserName, Email, ResponseURL);
+
+            ViewBag.UserName = UserName;
+            ViewBag.Email = Email;
+            ViewBag.ResponseURL = ResponseURL;
+
+            ViewBag.Result = result;
+
+            ViewBag.Action = result.Url;
+            ViewBag.Token = result.Token;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InscriptionFinish()
+        {
+            var token = Request.Form["TBK_TOKEN"];
+            var result = Inscription.Finish(token);
+
+
+            ViewBag.Token = token;
+            ViewBag.Result = result;
+            ViewBag.SaveToken = token;
+            ViewBag.SaveTbkUser = result.TransbankUser;
+
+            return View();
+        }
+    }
+}
