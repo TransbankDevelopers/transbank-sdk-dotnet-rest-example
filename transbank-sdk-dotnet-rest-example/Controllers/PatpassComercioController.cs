c265262160663b9500a1f3cdc1d63be9a3754abd
Nicolas Martinez <nmartinezv@icloud.com>
2019-09-12 21:52:37 -0300

    fix patpass commercio bad request

--- transbank-sdk-dotnet-rest-example/Controllers/PatpassComercioController.cs
+++ transbank-sdk-dotnet-rest-example/Controllers/PatpassComercioController.cs
@@ -6,49 +6,51 @@ namespace transbanksdkdotnetrestexample.Controllers
 {
     public class PatpassComercioController : Controller
     {
-        #region Patpass Comercio
         public ActionResult Start()
         {
-            var baseUrl = Request.Url.GetLeftPart(UriPartial.Authority);
-            var random = new Random();
             
-            var url = baseUrl + "/return";
+            var urlHelper = new UrlHelper(ControllerContext.RequestContext);
+
+            var returnUrl = urlHelper.Action("Start", "PatpassComercio", null, Request.Url.Scheme);
+
+            var random = new Random();
+
+            var url = returnUrl;
             var name = "nombre";
             var f_lastname = "pApellido";
             var s_lastname = "sApellido";
             var rut = "14959787-6";
             var service_id = random.Next(999999999).ToString();
-            var final_url = baseUrl + "/end";
-            var commerce_code = "28785666";
+            var final_url = urlHelper.Action("End", "PatpassComercio", null, Request.Url.Scheme);
+            var commerce_code = "28299257";
             var max_amount = random.Next(1000, 999999);
             var phone_number = random.Next(999999999).ToString();
             var mobile_number = random.Next(999999999).ToString();
             var patpass_name = "nombre del patpass";
-            var client_email = "persona@persona.cl";
+            var person_email = "persona@persona.cl";
             var commerce_email = "comercio@comercio.cl";
             var address = "huerfanos 101";
             var city = "city";
             
-            var urlHelper = new UrlHelper(ControllerContext.RequestContext);
-            var returnUrl = urlHelper.Action("Start", "PatpassComercio", null, Request.Url.Scheme);
 
             var result = Inscription.Start(
-                url: url,
-                name: name,
-                fLastname: f_lastname,
-                sLastname: s_lastname,
-                rut: rut,
-                serviceId: service_id,
-                finalUrl: final_url,
-                commerceCode: commerce_code,
-                maxAmount: max_amount,
-                phoneNumber: phone_number,
-                mobileNumber: mobile_number,
-                patpassName: patpass_name,
-                personEmail: client_email,
-                commerceEmail: commerce_email,
-                address: address,
-                city: city);
+                url,
+                name,
+                f_lastname,
+                s_lastname,
+                rut,
+                service_id,
+                final_url,
+                commerce_code,
+                max_amount,
+                phone_number,
+                mobile_number,
+                patpass_name,
+                person_email,
+                commerce_email,
+                address,
+                city
+                );
 
             ViewBag.ReturnUrl = returnUrl;
             ViewBag.Action = result.Url;
@@ -67,7 +69,7 @@ namespace transbanksdkdotnetrestexample.Controllers
             ViewBag.Phone_number = phone_number;
             ViewBag.Mobile_number = mobile_number;
             ViewBag.Patpass_name = patpass_name;
-            ViewBag.Client_name = client_email;
+            ViewBag.Client_name = person_email;
             ViewBag.Commerce_email = commerce_email;
             ViewBag.Address = address;
             ViewBag.City = city;
@@ -86,6 +88,12 @@ namespace transbanksdkdotnetrestexample.Controllers
 
             return View();
         }
+
+        public ActionResult End()
+        {
+
+            return View();
+        }
         
         [HttpPost]
         public ActionResult Status()
@@ -100,7 +108,5 @@ namespace transbanksdkdotnetrestexample.Controllers
 
             return View();
         }
-        
-        #endregion
     }
 }
